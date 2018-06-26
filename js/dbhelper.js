4841b6487220cf87c921e026775eb60a5ebc6b1f
Kat <lindakt16@gmail.com>
2018-06-25 23:05:59 -0500

    Mobile sizing for main page complete; started styling for individual pages

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 3000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    return `http://192.168.1.59:${port}/data/restaurants.json`;
   }
 
   /**
