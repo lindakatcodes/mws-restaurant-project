d96dc76fbb27aa9c0052f250e96ac23e415b2cdd
Kat <lindakt16@gmail.com>
2018-06-27 23:18:50 -0500

    Mobile styling is fixed and finished!

--- js/main.js
+++ js/main.js
@@ -74,7 +74,7 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 initMap = () => {
   self.newMap = L.map('map', {
         center: [40.722216, -73.987501],
-        zoom: 14,
+        zoom: 12,
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
@@ -208,4 +208,3 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 } */
-
