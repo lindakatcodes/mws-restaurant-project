5fe6d74d82826eb07aff9a19d3f2b8041703b8f2
Kat <lindakt16@gmail.com>
2018-06-29 23:29:08 -0500

    Responsive styling complete! Also added resize images function to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,27 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var imageMin = require('gulp-imagemin');
+var resize = require('gulp-image-resize');
+var rename = require('gulp-rename');
+var del = require('del');
 
+// Reduce images functions
+gulp.task('clean', function() {
+    return del('./img/optimized');
+});
+
+gulp.task('reduce-images', ['clean'], function() {
+    gulp.src('./img/*.{jpg, png}') 
+        .pipe(resize({ 
+            width: 800, 
+            quality: 0.5
+        }))
+        .pipe(imageMin()) 
+        .pipe(rename(function (path) { path.basename += "-optimized"; })) 
+        .pipe(gulp.dest('./img/optimized'))
+});
+
+// Watching functions
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
