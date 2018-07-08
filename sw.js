6f645215c94ae88855d956ade3d0e6e0624e7658
Kat <lindakt16@gmail.com>
2018-07-07 19:37:54 -0500

    More adjustments to service worker to cache as I'd like; minor styling changes

--- sw.js
+++ sw.js
@@ -1,17 +1,36 @@
-const currentCacheName = 'restaurant-reviews-v3';
+const currentCacheName = 'restaurant-reviews-v1';
 
 self.addEventListener('install', function(event) {
     console.log('installing service worker...');
     event.waitUntil(
         caches.open(currentCacheName).then(function(cache) {
             return cache.addAll([
-                '/index.html',
-                '/restaurant.html',
+                '/',
                 '/js/dbhelper.js',
                 '/js/main.js',
                 '/js/restaurant_info.js',
                 '/css/styles.css',
-                '/data/restaurants.json'
+                '/data/restaurants.json',
+                '/restaurant.html?id=1',
+                '/restaurant.html?id=2',
+                '/restaurant.html?id=3',
+                '/restaurant.html?id=4',
+                '/restaurant.html?id=5',
+                '/restaurant.html?id=6',
+                '/restaurant.html?id=7',
+                '/restaurant.html?id=8',
+                '/restaurant.html?id=9',
+                '/restaurant.html?id=10',
+                '/img/optimized/1-optimized.jpg',
+                '/img/optimized/2-optimized.jpg',
+                '/img/optimized/3-optimized.jpg',
+                '/img/optimized/4-optimized.jpg',
+                '/img/optimized/5-optimized.jpg',
+                '/img/optimized/6-optimized.jpg',
+                '/img/optimized/7-optimized.jpg',
+                '/img/optimized/8-optimized.jpg',
+                '/img/optimized/9-optimized.jpg',
+                '/img/optimized/10-optimized.jpg'
             ]);
         })
     );
